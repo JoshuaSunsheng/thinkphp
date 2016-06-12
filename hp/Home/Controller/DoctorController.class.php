bd1f0e9adcf78da486d5dfbbdddcac08cda05dbc
susunsheng <su371128017@163.com>
2016-06-13 00:16:49 +0800

    增加doctor路由

--- hp/Home/Controller/DoctorController.class.php
+++ hp/Home/Controller/DoctorController.class.php
@@ -10,8 +10,29 @@ namespace Home\Controller;
 
 use Think\Controller;
 
+define(CONTROLLER, __CONTROLLER__);
+
 class  DoctorController extends Controller{
+
+
+
+
     function myPatient(){
+        //获取系统常量, 并分组
+        //var_dump(get_defined_constants(true));
+        $this -> display();
+    }
+
+    function myStudy(){
+        $this -> display();
+    }
+
+
+    function dataImport(){
+        $this -> display();
+    }
+
+    function register(){
         $this -> display();
     }
 }
