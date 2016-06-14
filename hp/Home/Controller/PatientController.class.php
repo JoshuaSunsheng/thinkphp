49762a6aa00e45849801f48142bf126148840ddb
susunsheng <su371128017@163.com>
2016-06-14 23:25:21 +0800

    重新整理病人 application表达字段

--- hp/Home/Controller/PatientController.class.php
+++ hp/Home/Controller/PatientController.class.php
@@ -10,6 +10,49 @@ namespace Home\Controller;
 
 use Think\Controller;
 
+define(CONTROLLER, __CONTROLLER__);
+
 class PatientController extends Controller{
+
+    /*
+     * 病人第一次注册登录
+     * */
+    function login(){
+        //获取系统常量, 并分组
+        //var_dump(get_defined_constants(true));
+        $this -> display();
+    }
+
+    /*
+     * 病人第一次注册登录
+     * 知情同意书
+     * */
+    function agreement(){
+        //获取系统常量, 并分组
+        //var_dump(get_defined_constants(true));
+        $this -> display();
+    }
+
+    /*
+     * 病人第一次注册登录
+     * 填写表格
+     * */
+    function application(){
+        //获取系统常量, 并分组
+        //var_dump(get_defined_constants(true));
+        $this -> display();
+    }
+
+    /*
+     * 病人第一次注册登录
+     * 预约医生
+     * */
+    function appointment(){
+        //获取系统常量, 并分组
+        //var_dump(get_defined_constants(true));
+        $this -> display();
+    }
+
+
 
 }
