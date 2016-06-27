069831968ded97c50b7f21014e4068f3ec891f09
leosunsheng <su371128017@qq.com>
2016-06-27 17:16:12 +0800

    关联模型

--- hp/Home/Model/PatientModel.class.php
+++ hp/Home/Model/PatientModel.class.php
@@ -2,10 +2,18 @@
 
 
 namespace Home\Model;
-use Think\Model;
+use Think\Model\RelationModel;
 
-class PatientModel extends Model{
-
+class PatientModel extends RelationModel{
+    protected $_link = array(
+        'Patient' => array(
+            'mapping_type' => self::HAS_ONE,
+            'class_name' => 'Patient',
+            'mapping_name' => 'Patients',
+            'foreign_key' => 'userId',
+            // 定义更多的关联属性
+        ),
+    );
 
 
 }
