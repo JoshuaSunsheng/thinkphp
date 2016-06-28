d8295e57f3ca289516823ebcde936b0e7c36b458
susunsheng <su371128017@163.com>
2016-06-28 08:49:56 +0800

    get 请求成功访问

--- hp/Common/Common/function.php
+++ hp/Common/Common/function.php
@@ -28,13 +28,16 @@
         $httph =curl_init($url);
         curl_setopt($httph, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($httph, CURLOPT_SSL_VERIFYHOST, 1);
-        curl_setopt($httph,CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($httph, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-
         curl_setopt($httph, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($httph, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
         curl_setopt($httph, CURLOPT_HEADER,1);
+        // header:1 返回头部信息, 0: 致返回数据结构
+
+        //curl_multi_getcontent( resource $httph );
+
         $rst=curl_exec($httph);
         curl_close($httph);
+
         return $rst;
     }
     /*
