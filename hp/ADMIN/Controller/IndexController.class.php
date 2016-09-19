94b18dca38d949b95997b0a5d97edf48ec1c5537
susunsheng <su371128017@163.com>
2016-09-20 01:10:17 +0800

    1.3 实现登录登出

--- hp/ADMIN/Controller/IndexController.class.php
+++ hp/ADMIN/Controller/IndexController.class.php
@@ -12,12 +12,33 @@ use Think\Controller;
 
 define(CONTROLLER, __CONTROLLER__);
 
-class  IndexController extends Controller{
-    function index(){
+class  IndexController extends Controller
+{
+    function index()
+    {
         //获取系统常量, 并分组
         //var_dump(get_defined_constants(true));
+        if (empty($_POST)) {
+            $loginName = session('loginName'); //授权信息
+            if ($loginName) {
+                var_dump($loginName);
+                $this->display();
+            } else {
+                redirect(CONTROLLER . '/../Login/login', 2, '您已经登出, 请重新登录');
+            }
+            return;
+        }
+    }
 
-        $this -> display();
+
+    function logout()
+    {
+        session('loginName', null); //保存授权信息
+        $loginName = session('loginName'); //授权信息
+
+        $ret['rescode'] = "00";
+        $ret['msg'] = "登出成功".$loginName;
+        $this->ajaxReturn($ret);
     }
 
 }
---
+++ hp/ADMIN/Controller/LoginController.class.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: susunsheng
+ * Date: 16/9/19
+ * Time: 下午11:41
+ */
+
+namespace Admin\Controller;
+
+use Think\Controller;
+
+define(CONTROLLER, __CONTROLLER__);
+
+class  LoginController extends Controller{
+
+
+
+    /**
+     * @param $options
+     * @param $authnum
+     * @throws \Org\Com\Exception
+     */
+    public function login(){
+
+        if (empty($_POST)) {
+            $loginName = session('loginName'); //保存授权信息
+            if($loginName){
+                redirect(CONTROLLER . '/../Index/index', 2, '您已经登录, 页面跳转中...');
+            }
+
+            $this -> display();
+            return;
+        }
+
+        //短信验证码（模板短信）,默认以65个汉字（同65个英文）为一条（可容纳字数受您应用名称占用字符影响），超过长度短信平台将会自动分割为多条发送。分割后的多条短信将按照具体占用条数计费。
+        $loginName = $_POST['loginName'];
+        $password = $_POST['password'];
+
+        //echo $phoneNumber;
+        //echo $code;
+        $User = M("User");
+        //echo $Msgcode;
+        $data = $User->where('phoneNumber=%s', $loginName)->order('CREATETIME desc')->limit(1)->find();
+        //echo $data;
+//        var_dump($data);
+        if(!$data){
+            $ret['rescode'] = "01";
+            $ret['msg'] = "用户不存在";
+        }
+        else if($data["password"] != $password ){
+            $ret['rescode'] = "01";
+            $ret['msg'] = "用户名或密码错误".$data["password"];
+        }
+        else{
+            session('loginName',$loginName); //保存授权信息
+
+            $ret['rescode'] = "00";
+            $ret['msg'] = "验证通过";
+        }
+
+        $this->ajaxReturn($ret);
+
+    }
+
+
+
+}
