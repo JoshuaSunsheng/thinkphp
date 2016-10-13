0e355f6704bfa600fe8b2a044728731dcafe65ed
susunsheng <su371128017@163.com>
2016-10-13 23:03:10 +0800

    2.0

--- hp/ADMIN/Controller/DoctorController.class.php
+++ hp/ADMIN/Controller/DoctorController.class.php
@@ -38,7 +38,10 @@ class  DoctorController extends Controller
 //        dump($map);
 
         //利用page函数。来进行自动的分页
-        $data = $db->page($page, $pagesize)
+        $data = $db->alias('a')->page($page, $pagesize)
+            ->join('__DICT_STATUS__ as status ON status.id = a.status')
+            ->field('a.*,a.status as statusCode,
+                 status.title as status')
             ->where($map)
             ->select();
         $recordnum = $db->page($page, $pagesize)
@@ -106,41 +109,13 @@ class  DoctorController extends Controller
 
 
     /*
-     * 预约信息
+     * 预约信息-后台显示
      * */
     function appointment($queryStr = '', $page = 1, $pagesize = 10)
     {
         \Think\Log::write('login appointment:', "INFO");
+        list($data, $recordnum, $pagenum) = $this->innerAppointment($queryStr, $page, $pagesize);
 
-//        $db=D("Appointment");   //D方法无效
-        $db = new AppointmentModel();
-
-        //使用map作为查询条件,混合模式
-        $where['patient.realname'] = array('like', '%' . $queryStr . '%');
-        $where['doctor.realname'] = array('like', '%' . $queryStr . '%');
-        $where['_logic'] = 'or';
-        $map['_complex'] = $where;
-//        dump($map);
-
-        //利用page函数。来进行自动的分页
-        $data = $db->alias('a')->page($page, $pagesize)->join('__DOCTOR__ as doctor ON doctor.id = a.doctor_id')
-            ->join('__PATIENT__ as patient ON a.patient_id = patient.id')
-            ->field('a.id, a.begindate, a.enddate, patient.realname as patientname, doctor.realname as doctorname')
-            ->where($map)
-            ->select();
-        $recordnum = $db->alias('a')->page($page, $pagesize)->join('__DOCTOR__ as doctor ON doctor.id = a.doctor_id')
-            ->join('__PATIENT__ as patient ON a.patient_id = patient.id')
-            ->field('a.id, a.begindate, a.enddate, patient.realname as patientname, doctor.realname as doctorname')
-            ->where($map)
-            ->count();
-
-        //计算分页
-        $pagenum = $recordnum / $pagesize;
-        //如果不能整除，则自动加1页
-        if ($pagenum % 1 !== 0) {
-            \Think\Log::write('login record pagenum: '.$pagenum, "INFO");
-            $pagenum = (int)$pagenum + 1;
-        }
 
 //        var_dump(($data),true);
         \Think\Log::write('login write', 'WARN' . $data);
@@ -157,13 +132,24 @@ class  DoctorController extends Controller
 
     }
 
-    //删除预约
-    function delete($id)
+    //审核通过预约
+    function passAppointment($id)
     {
-        \Think\Log::record('delete record:'.$id);
+        \Think\Log::record('passAppointment record:'.$id);
         $db = new AppointmentModel();
-        $db->where('id=' . $id)->delete();
-        \Think\Log::record('delete record end');
+//        $db->where('id=' . $id)->delete();
+        $db->where('id=' . $id)->setField('status',APPOINTMENT_PASS);
+        \Think\Log::record('passAppointment record end');
+    }
+
+    //审核失败预约
+    function cancelAppointment($id)
+    {
+        \Think\Log::record('cancelAppointment record:'.$id);
+        $db = new AppointmentModel();
+//        $db->where('id=' . $id)->delete();
+        $db->where('id=' . $id)->setField('status',APPOINTMENT_FAIL);
+        \Think\Log::record('cancelAppointment record end');
     }
 
     //删除医生
@@ -173,6 +159,88 @@ class  DoctorController extends Controller
         $db->where('id=' . $id)->delete();
         \Think\Log::record('deleteDoctor record end');
     }
+
+    //审核通过医生
+    function passDoctor($id){
+        \Think\Log::record('deleteDoctor record:'.$id);
+        $db = new DoctorModel();
+//        $db->where('id=' . $id)->delete();
+        $db->where('id=' . $id)->setField('status',DOCTOR_PASS);
+
+        \Think\Log::record('deleteDoctor record end');
+    }
+
+    //审核失败医生
+    function cancelDoctor($id){
+        \Think\Log::record('deleteDoctor record:'.$id);
+        $db = new DoctorModel();
+        $db->where('id=' . $id)->setField('status',DOCTOR_FAIL);
+
+        $db->where('id=' . $id)->delete();
+        \Think\Log::record('deleteDoctor record end');
+    }
+
+    /**
+     * 预约信息查询
+     * @param $queryStr
+     * @param $page
+     * @param $pagesize
+     * @param $where
+     * @param $map
+     * @return array
+     */
+    public function innerAppointment($queryStr, $page, $pagesize)
+    {
+        \Think\Log::write('innerAppointment page: ' . $page, "INFO");
+
+//        $db=D("Appointment");   //D方法无效
+        $db = new AppointmentModel();
+
+        //使用map作为查询条件,混合模式
+        $where['patient.realname'] = array('like', '%' . $queryStr . '%');
+        $where['doctor.realname'] = array('like', '%' . $queryStr . '%');
+        $where['_logic'] = 'or';
+        $map['_complex'] = $where;
+//        dump($map);
+
+//        $data = $db->alias('a')->page($page, $pagesize)
+//            ->join('__DICT_STATUS__ as status ON status.id = a.status')
+//            ->field('a.*,a.status as statusCode,
+//                 status.title as status')
+//            ->where($map)
+//            ->select();
+
+        //利用page函数。来进行自动的分页
+        $data = $db->alias('a')->page($page, $pagesize)->join('__DOCTOR__ as doctor ON doctor.id = a.doctor_id')
+            ->join('__PATIENT__ as patient ON a.patient_id = patient.id')
+            ->join('__DICT_NEGATIVE_POSITIVE__ as np ON np.id = patient.checkStatus')
+            ->join('__DICT_NOTIFY__ as notify ON notify.id = a.notify')
+            ->join('__DICT_STATUS__ as status ON status.id = a.status')
+            ->field('a.id, a.begindate, a.enddate, patient.realname as patientname, doctor.realname as doctorname,status.title as status,a.status as statusCode, np.title as checkstatus, notify.title as notify, patient.birthday as birthday')
+            ->where($map)
+            ->select();
+        $recordnum = $db->alias('a')->page($page, $pagesize)->join('__DOCTOR__ as doctor ON doctor.id = a.doctor_id')
+            ->join('__PATIENT__ as patient ON a.patient_id = patient.id')
+            ->join('__DICT_NEGATIVE_POSITIVE__ as np ON np.id = patient.checkStatus')
+            ->join('__DICT_NOTIFY__ as notify ON notify.id = a.notify')
+            ->join('__DICT_STATUS__ as status ON status.id = a.status')
+            ->field('a.id, a.begindate, a.enddate, patient.realname as patientname, doctor.realname as doctorname,status.title as status,a.status as statusCode')
+            ->where($map)
+            ->count();
+
+        //计算分页
+        $pagenum = $recordnum / $pagesize;
+        //如果不能整除，则自动加1页
+        if ($pagenum % 1 !== 0) {
+            \Think\Log::write('login record pagenum: ' . $pagenum, "INFO");
+            $pagenum = (int)$pagenum + 1;
+        }
+
+        return array($data, $recordnum, $pagenum);
+    }
+
+
+
     /* function table($page=1,$pagesize=20){
          $db=M("user");
          $recordnum = $db->count();
